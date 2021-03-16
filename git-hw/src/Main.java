43e6a7bdb820640881aea4f1b2e94c0b4394c0b5
ParsaJahantab <79784868+ParsaJahantab@users.noreply.github.com>
2021-03-16 21:50:16 +0330

    Add files via upload

---
+++ git-hw/src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Hello git");
+        System.out.println("My name is: ParsaJahantab");
+        System.out.println("My student ID is 99243028");
+    }
+}  
